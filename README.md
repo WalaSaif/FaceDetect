d9de07c15ea188d7fdb1cde4fce59867a69d9579
Wala <>
2020-11-28 15:38:04 +0400

    Detect/Move Multiple Images

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-Run the code like this:
+Program reads multiple images from a folder and separates them into two folders (Face Detected/Not Detected)
 
-*python face_detect.py abba.png*
+To run code:
+- Set image destination folders path in lines 9,10
+- run *python3 face_detect_cv3.py images*
 
-If you want to understand how the code works, the details are here:
-
+If you want to understand how the code works, the detaisls are here:
 https://realpython.com/blog/python/face-recognition-with-python/
 
-
 Update: Now supports OpenCV3. This change has been made by furetosan ( https://github.com/furetosan) and tested on Linux.
-
-To run the OpenCV3 version, run facedetect_cv3.py.
--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -1,31 +1,48 @@
+from os import listdir
+from os.path import isfile, join
+import numpy
 import cv2
 import sys
+from pathlib import Path
+
+# New image destination folders
+facesPath = "Faces"
+noFacesPath = "NoFaces"
 
 # Get user supplied values
-imagePath = sys.argv[1]
+folderPath = sys.argv[1]
+imagefiles = [ f for f in listdir(folderPath) if isfile(join(folderPath,f)) ]
+images = numpy.empty(len(imagefiles), dtype=object)
 cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+for n in range(0, len(imagefiles)):
+    if not imagefiles[n].lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')): continue
+    images[n] = cv2.imread( join(folderPath,imagefiles[n]) )
+    gray = cv2.cvtColor(images[n], cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
+    print("Found {0} faces!".format(len(faces)))
 
-print("Found {0} faces!".format(len(faces)))
+    # Modify image path
+    if len(faces) != 0: Path(folderPath + "/" + imagefiles[n]).rename(facesPath + "/" + imagefiles[n])
+    else: Path(folderPath + "/" + imagefiles[n]).rename(noFacesPath + "/" + imagefiles[n])
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Draw a rectangle around the faces and display image window
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(images[n], (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    # cv2.imshow("Faces found", images[n])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.waitKey(1)
